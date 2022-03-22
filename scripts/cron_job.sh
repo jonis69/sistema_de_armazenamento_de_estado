438c860901b41c5030c0277f2a8076a68a9cda9d
Jonas <jonas@HACKING-MACHINE>
2022-03-22 07:06:33 -0300

    Mudei a ordem e a lógica dos cronjobs. Agora deve funcionar corretamente

--- scripts/cron_job.sh
+++ scripts/cron_job.sh
@@ -4,8 +4,8 @@ DIRNAME=$(pwd)
 cd ${DIRNAME}
 
 sudo crontab -l > cron_bkp
+sudo echo "*/2 7-23 * * * sudo /usr/bin/sh /var/www/scripts/gerar_log.sh" >> cron_bkp
+sudo echo "*/2 23 * * * sudo /usr/bin/sh /var/www/scripts/gerar_log.sh" >> cron_bkp
 sudo echo "*/1 0-6 * * * sudo /usr/bin/sh /var/www/scripts/gerar_html_manutencao.sh" >> cron_bkp
-sudo echo "0 7 * * * sudo /usr/bin/sh /var/www/scripts/gerar_html_manutencao.sh" >> cron_bkp
-sudo echo "*/2 7 * * * sudo /usr/bin/sh /var/www/scripts/gerar_log.sh" >> cron_bkp
 sudo crontab cron_bkp
 sudo rm cron_bkp
